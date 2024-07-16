b46a88c2f258c3f53fc03996286f2de4624c2336
lehenkyi-yevhen-ar <meister.ramires@gmail.com>
2024-07-16 18:52:09 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import SimpleLightbox from "simplelightbox";
+// import SimpleLightbox from "simplelightbox";
 
-import "simplelightbox/dist/simple-lightbox.css"
+// import "simplelightbox/dist/simple-lightbox.css"
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     const lightbox = new SimpleLightbox('.gallery a', {
